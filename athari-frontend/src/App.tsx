e1171640654e550956cfe9b94953195305aed7c0
Steve Blondo <ndjouonkep@gmail.com>
2025-12-08 15:47:19 +0100

    feat: integrate Tailwind CSS and refactor layout components
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed existing CSS styles and replaced them with Tailwind CSS utility classes in App.tsx and App.css.
    - Created new Header and Sidebar components with responsive design using Tailwind CSS.
    - Updated App.tsx to include the new Header and Sidebar components for layout structure.
    - Deleted unused index.css file to streamline styles.

--- athari-frontend/src/App.tsx
+++ athari-frontend/src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Sidebar from './components/Layout/Sidebar'
+import Header from './components/Layout/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-56
+dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500'>
+      <div className='flex h-screen overflow-hidden'>
+        <Sidebar />
+        <div className='flex-1 flex flex-col overflow-hidden'>
+          <Header />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 

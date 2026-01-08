887de7cd8b77716bc27a1922786070a5145bf1fd
vanellakenfack <vanellakenfack00@gmail.com>
2026-01-08 10:50:04 +0100

    caisse

--- athari-backend/database/migrations/2026_01_08_084000_create_caisse_sessions_table.php
+++ athari-backend/database/migrations/2026_01_08_084000_create_caisse_sessions_table.php
@@ -15,10 +15,19 @@ return new class extends Migration
             $table->id();
     $table->foreignId('guichet_session_id')->constrained('guichet_sessions')->onDelete('cascade');
     $table->foreignId('caissier_id')->constrained('users');
+    $table->string('code_caisse')->index(); // Essentiel pour getDernierSoldeFermeture()
     $table->decimal('solde_ouverture', 15, 2)->default(0);
     $table->decimal('solde_fermeture', 15, 2)->nullable();
-    $table->enum('statut', ['OU', 'FE'])->default('FE');
+    $table->enum('statut', ['OU', 'FE','RE'])->default('FE');
     $table->json('billetage_ouverture')->nullable(); // Détail des billets (10k, 5k, etc.)
+
+        $table->json('billetage_fermeture')->nullable(); // Important pour l'ajustage final
+        
+        // Temps
+        $table->timestamp('heure_ouverture')->useCurrent();
+        $table->timestamp('heure_fermeture')->nullable();
+        
+  
     $table->decimal('solde_informatique', 15, 2)->default(0); // Ce que le système dit
     $table->decimal('solde_physique', 15, 2)->default(0);     
     $table->text('observations')->nullable(); // Pour les écarts de caisse
